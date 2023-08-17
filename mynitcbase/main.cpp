ea4085fdb23f883f7f2644d4720c38304f06c506
VENKATASAI24 <venkatasai24@localhost.localdomain>
2023-08-17 15:04:46 +0530

    stage1_q

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -9,16 +9,11 @@ int main(int argc, char *argv[]) {
   Disk disk_run;
 
   unsigned char buffer[BLOCK_SIZE];
-  Disk::readBlock(buffer, 7000);
-  char message[] = "hello";
-  memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
-
-  unsigned char buffer2[BLOCK_SIZE];
-  char message2[6];
-  Disk::readBlock(buffer2, 7000);
-  memcpy(message2, buffer2 + 20, 6);
-  std::cout << message2;
+  for(int j=0;j<=3;j++) {
+    Disk::readBlock(buffer,j);
+    for(int i=0;i<BLOCK_SIZE;i++) std::cout<<(int)buffer[i]<<" ";
+    std::cout<<std::endl;
+  }
 
   return 0;
 }
