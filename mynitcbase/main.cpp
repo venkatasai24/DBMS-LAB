8a90db9d35d6652dc56017d51a14da8269e2148a
VENKATASAI24 <venkatasai24@localhost.localdomain>
2023-08-19 17:25:41 +0530

    stage2 q1 and q2

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -7,9 +7,56 @@
 #include <iostream>
 using namespace std;
 
+<<<<<<< HEAD
 int main(int argc, char *argv[])
 {
   Disk disk_run;
+=======
+void updateAttr(char relName[], char oldAttr[], char updAttr[])
+{
+
+  // get attribute catalogue block and it's header
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+  HeadInfo attrCatHeader;
+
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  // search through all attribute records
+  for (int i = 0; i < attrCatHeader.numEntries; i++)
+  {
+    // declare attrCatRecord
+    Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+    // now get the records one by one
+    attrCatBuffer.getRecord(attrCatRecord, i);
+
+    /* attribute catalog entry corresponds to the current relation */
+    if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relName) == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, oldAttr) == 0)
+    {
+      strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, updAttr);
+      attrCatBuffer.setRecord(attrCatRecord, i);
+      std::cout << "Updated\n";
+      break;
+    }
+    if (i == attrCatHeader.numSlots - 1)
+    {
+      // re initializing the slot index
+      i = -1;
+      // updating the buffer block with next block;
+      attrCatBuffer = RecBuffer(attrCatHeader.rblock);
+      // updating the header
+      attrCatBuffer.getHeader(&attrCatHeader);
+    }
+  }
+}
+
+int main(int argc, char *argv[])
+{
+
+  Disk disk_run;
+  char rel_name[16] = "Students", old_attr[16] = "Class", upd_attr[16] = "Batch";
+  updateAttr(rel_name, old_attr, upd_attr);
+>>>>>>> stage2_q2
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
@@ -39,12 +86,17 @@ int main(int argc, char *argv[])
       Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
       attrCatBuffer.getRecord(attrCatRecord, noOfAttrs);
 
+<<<<<<< HEAD
+=======
+      /* attribute catalog entry corresponds to the current relation */
+>>>>>>> stage2_q2
       if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
       {
         const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
         printf("  %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
       }
-      if(noOfAttrs == attrCatHeader.numSlots - 1) {
+      if (noOfAttrs == attrCatHeader.numSlots - 1)
+      {
         // re initializing the slot index
         noOfAttrs = -1;
         // updating the buffer block with next block;
