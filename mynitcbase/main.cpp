b52d2fbf9d93ed7ed8fa8a17fb3902c3a951c8c6
VENKATASAI24 <venkatasai24@localhost.localdomain>
2023-09-02 15:00:55 +0530

    stage2 q2 commented

--- mynitcbase/main.cpp
+++ mynitcbase/main.cpp
@@ -7,9 +7,53 @@
 #include <iostream>
 using namespace std;
 
+void updateAttr(char relName[], char oldAttr[], char updAttr[])
+{
+
+  // get attribute catalogue block and it's header
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+  HeadInfo attrCatHeader;
+
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  // search through all attribute records
+  for (int i = 0; i < attrCatHeader.numEntries; i++)
+  {
+    // declare attrCatRecord
+    Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+    // now get the records one by one
+    attrCatBuffer.getRecord(attrCatRecord, i);
+
+    /* attribute catalog entry corresponds to the current relation */
+    if (strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relName) == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, oldAttr) == 0)
+    {
+      strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, updAttr);
+      attrCatBuffer.setRecord(attrCatRecord, i);
+      std::cout << "Updated\n";
+      break;
+    }
+    if (i == attrCatHeader.numSlots - 1)
+    {
+      // re initializing the slot index
+      i = -1;
+      // updating the buffer block with next block;
+      attrCatBuffer = RecBuffer(attrCatHeader.rblock);
+      // updating the header
+      attrCatBuffer.getHeader(&attrCatHeader);
+    }
+  }
+}
+
 int main(int argc, char *argv[])
 {
   Disk disk_run;
+
+  /*
+  to update attribue
+  char rel_name[16] = "Students", old_attr[16] = "Class", upd_attr[16] = "Batch";
+  updateAttr(rel_name, old_attr, upd_attr);
+  */
 
   // create objects for the relation catalog and attribute catalog
   RecBuffer relCatBuffer(RELCAT_BLOCK);
